3a02b672e778e6f7b4c5c6498822057ddb3af21d
simiajani <148916768+simiajani@users.noreply.github.com>
2025-04-21 03:41:21 -0400

    Update TaskDashboard.js

--- frontend/src/components/TaskDashboard.js
+++ frontend/src/components/TaskDashboard.js
@@ -1,1 +1,38 @@
+import React, {useState, useEffect} from 'react';
+import axios from 'axios';
+import TaskForm from './TaskForm';
+import TaskList from './TaskList';
 
+//manages all task state and coordinates between TaskForm and TaskList
+function TaskDashboard(){
+  const [tasks, setTasks] = useState([]);
+
+  //gets tasks from backend when component mounts
+  useEffect(() => {
+    getTasks();
+  }, []);
+
+  const getTasks = async () => {
+    const response = await.get('http://localhost:3000/api/tasks');
+    setTasks(response.data);
+  };
+
+  const addTask = async (task) => {
+    await axios.post('http://localhost:3000/api/tasks', task);
+    getTasks(); //get the task again after adding it
+  };
+
+  const deleteTask = async (id) => {
+    await axios.delete('http://localhost:3000/api/tasks/${id}');
+    getTasks(); //get the tasks again after deleting
+  };
+
+  return (
+    <div>
+      <TaskForm onAddTask={addTask} />
+      <TaskList tasks={tasks} onDeleteTask={deleteTask} />
+    </div>
+  );
+}
+
+export default TaskDashboard;
