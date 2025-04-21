3bb9d308a99d5d4b739f819ba65c20ac645719c6
simiajani <148916768+simiajani@users.noreply.github.com>
2025-04-21 03:31:24 -0400

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,1 +1,14 @@
+import React from 'react';
+import TaskDashboard from '.components/TaskDashboard';
 
+//top-level component rendering the task dashboard
+function App() {
+  return (
+    <div>
+      <h1>Objective Organizer</h1>
+      <TaskDashboard />
+    </div>
+  );
+}
+
+export default App;
