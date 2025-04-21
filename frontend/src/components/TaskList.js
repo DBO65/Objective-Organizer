fe0093ee9000e5c4c86c86162905fc2b8c7583e2
simiajani <148916768+simiajani@users.noreply.github.com>
2025-04-21 04:18:03 -0400

    Update TaskList.js

--- frontend/src/components/TaskList.js
+++ frontend/src/components/TaskList.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 //displays a sorted list of tasks
-function (TaskList({tasks, onDeleteTask}){
+function (TaskList({tasks, onDeleteTask, onEditTask}){
   //sort tasks descending by priority (3 is the highest)
   const sorted = [...tasks].sort((a, b) => b.priority - a.priority);
 
@@ -11,8 +11,11 @@ function (TaskList({tasks, onDeleteTask}){
           <li key={task._id}>
           {task.title} (Priority: {task.priority})
           <button onClick={() => onDeleteTask(task._id)}>Delete</button>
+          <button onClick={() => onEditTask(task}>Edit</button>
        </li>
      ))}
    </ul>
  );
 }
+
+export default TaskList;
