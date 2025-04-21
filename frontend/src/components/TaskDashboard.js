f01fa6eb277c1d3a5682eb71cbf5aa25616a42f6
simiajani <148916768+simiajani@users.noreply.github.com>
2025-04-21 04:06:06 -0400

    Update TaskDashboard.js

--- frontend/src/components/TaskDashboard.js
+++ frontend/src/components/TaskDashboard.js
@@ -6,6 +6,8 @@ import TaskList from './TaskList';
 //manages all task state and coordinates between TaskForm and TaskList
 function TaskDashboard(){
   const [tasks, setTasks] = useState([]);
+  const [taskToEdit, setTaskToEdit] = useState(null); //new state for editing
+  const [error, setError] = useState(null); //for error handling
 
   //gets tasks from backend when component mounts
   useEffect(() => {
@@ -13,26 +15,60 @@ function TaskDashboard(){
   }, []);
 
   const getTasks = async () => {
-    const response = await.get('http://localhost:3000/api/tasks');
+    try {
+    const response = await axios.get('http://localhost:3000/api/tasks');
     setTasks(response.data);
-  };
+  } catch (err) {
+    setError('Failed to load tasks');
+  }  
+};
 
   const addTask = async (task) => {
+    try {
     await axios.post('http://localhost:3000/api/tasks', task);
     getTasks(); //get the task again after adding it
+    } catch (err) {
+      setError('Failed to add task');
+    }
   };
 
   const deleteTask = async (id) => {
+    try {
     await axios.delete('http://localhost:3000/api/tasks/${id}');
     getTasks(); //get the tasks again after deleting
+    } catch (err) {
+      setError('Failed to add task');
+    }
+  };
+
+  const startEditTask = (task) => {
+    setTaskToEdit(task); //pass task to the form
+  };
+
+  const updateTask = async (updatedTask) => {
+    try {
+      await axios.delete('http://localhost:3000/api/tasks/${updatedTask._id}', updatedTask);
+    setTaskToEdit(null); //clear edit mode
+    } catch (err) {
+      setError('Failed to update task');
+    }
   };
 
   return (
     <div>
-      <TaskForm onAddTask={addTask} />
-      <TaskList tasks={tasks} onDeleteTask={deleteTask} />
-    </div>
-  );
+    {error && <div style={{color: 'red'}}>{error}</div>}
+    <TaskForm 
+      onAddTask={addTask}
+      onUpdateTask={updateTask}
+      taskToEdit={taskToEdit}
+    />
+    <TaskList 
+        tasks={tasks} 
+        onDeleteTask={deleteTask}
+        onEditTask={startEditTask}
+    />
+  </div>
+);
 }
 
 export default TaskDashboard;
