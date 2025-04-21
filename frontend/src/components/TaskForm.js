8be2ee0f51c5d8834b4ad08c28ac5aebda994fc9
simiajani <148916768+simiajani@users.noreply.github.com>
2025-04-21 04:15:27 -0400

    Update TaskForm.js

--- frontend/src/components/TaskForm.js
+++ frontend/src/components/TaskForm.js
@@ -5,11 +5,30 @@ function TaskForm({onAddTask}) {
   const [title, setTitle] = usetate('');
   const [priority, setPriority] = useState(1); //default priority
 
+  useEffect(() => {
+    if (taskToEdit) {
+      setTitle(taskToEdit.title);
+      setPriority(taskToEdit.priority);
+    } else {
+      setTitle('');
+      setPriority(1);
+    }
+  }, [taskToEdit]);
+
   const handleSubmit = (e) => {
     e.preventDefault();
-    if (!title.trim()) return;
-
-    onAddTask({title, priority});
+    const taskData;
+      title,
+      priority,
+      ...(taskToEdit && {_id: taskToEdit._id}) //include id if editing
+  };
+     
+    if (taskToEdit){
+      onUpdateTask(taskData);
+    } else {
+      onAddTask(taskData);
+    }
+     
     setTitle('');
     setPriority(1); //reset after submit
   };
@@ -27,7 +46,11 @@ function TaskForm({onAddTask}) {
         <option value={2}>Medium</option>
         <option value={3}>High</option>
       </select>
-      <button type="submit">Add Task</button>
+      <button type="submit">
+          {taskToEdit ? 'Update Task' : 'Add Task'}
+      </button>
     </form>
   );
 }
+
+export default TaskForm;
