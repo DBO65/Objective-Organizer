6ad02354c743aece562488277dcafa572466229e
simiajani <148916768+simiajani@users.noreply.github.com>
2025-04-22 01:17:31 -0400

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,19 +3,20 @@ import TaskDashboard from './components/TaskDashboard';
 
 function App() {
   return (
-    <div style={{ padding: '2rem' }}>
+    <div style={{ padding: '2rem', backgroundColor: '#ffe0b2', minHeight: '100vh' }}>
       {/* Heading Section */}
       <div style={{ textAlign: 'center', marginBottom: '2rem' }}>
         <h1 style={{
           fontSize: '2rem',
           fontWeight: '600',
-          marginBottom: '0.3rem'
+          marginBottom: '0.3rem',
+          color: '#0021A5'
         }}>
           Objective Organizer
         </h1>
         <p style={{
           fontSize: '1rem',
-          color: '#000'
+          color: '#0021A5'
         }}>
           Crush Your Goals, One Task at a Time!
         </p>
@@ -28,5 +29,6 @@ function App() {
 }
 
 export default App;
+
 
 
