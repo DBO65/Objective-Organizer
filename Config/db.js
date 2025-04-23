d319e97946f4d6d13ece34a64138686855bd839e
David-Benjamin A. Ogilvie <73730800+DBO65@users.noreply.github.com>
2025-04-22 20:35:25 -0400

    Update db.js
    Just added comments

--- Config/db.js
+++ Config/db.js
@@ -1,12 +1,15 @@
-import mongoose from 'mongoose';
+import mongoose from 'mongoose';                                        // Import the Mongoose library for MongoDB object modeling
 
-
+                                                                        // Asynchronous function to establish a connection to the MongoDB database
 export const connectDB = async () => {
     try {
+                                                                        // Use the MONGO_URI environment variable to connect
         const conn = await mongoose.connect(process.env.MONGO_URI);
+
+                                                                        // If successful, log the host to which we are connected
         console.log(`MongoDB Connected: ${conn.connection.host}`);
-    } catch (error) {
+    } catch (error) {                                                    // If there's an error during connection, log the error message
         console.error(`Error: ${error.message}`);
-        process.exit(1);  // 1 means exit with failure and 0 means success
+        process.exit(1);                                                 // 1 means exit with failure and 0 means success
     }
 };
